fc0ddeec76c25f365586eabc84bf677f0378fcf1
stamenkovdmitriy <111305109+stamenkovdmitriy@users.noreply.github.com>
2023-06-27 12:06:00 +0300

    Добавьте файлы проекта.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Maui_Adaptiv;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Maui_Adaptiv;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
